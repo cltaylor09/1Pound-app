1a6cbb4dd0eae79d4598bf1884d8d8cd7ee8ce94
cltaylor09 <your_email@example.com>
2022-10-09 13:21:09 -0400

    Added calendar edit component

--- src/app/calendar-editor/calendar-editor.component.ts
+++ src/app/calendar-editor/calendar-editor.component.ts
@@ -21,12 +21,10 @@ export class CalendarEditorComponent implements OnInit {
     muscle: new FormControl(''),
     time: new FormControl(''),
     activity: new FormControl('', Validators.required)
-  
   });
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.calendarForm.value);
   }
-
 }
